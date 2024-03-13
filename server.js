47731f11065e0c5d0e444894e5191cc87f4a7b2c
Ravi Tambade <ravi.tambade@transflower.in>
2024-03-13 12:13:10 +0530

    web server code

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.end('<h1>Transflower Store</h1>');
 });
-server.listen(7000, hostname, () => {
+server.listen(7000, () => {
   console.log(`Server running at 7000`);
 });
